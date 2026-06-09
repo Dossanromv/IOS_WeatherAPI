151f885eb1241def726b080fa568781b3d123625
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-06-09 11:31:19 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Rakhat Dossanbayev on 09.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

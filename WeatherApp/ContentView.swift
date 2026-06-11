e2c1d28fe0d882d287b44a7f2041f470712392be
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-06-11 15:30:27 +0500

    I added some text on app

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -4,16 +4,44 @@
 //
 //  Created by Rakhat Dossanbayev on 09.06.2026.
 //
-
+import Foundation
+import Combine
 import SwiftUI
 
+
+
+struct WeatherResponse: Codable {
+    let main: Main
+    let weather: [Weather]
+    let name: String
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
+
+
+
 struct ContentView: View {
+    
+    @State private var temperature = "_"
+    
+    
     var body: some View {
+        Text("Погода загружается...")
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("🌤 Shymkent")
+                .font(.largeTitle)
+                .bold()
+            Text(temperature)
+                .font(.system(size:60))
+            
         }
         .padding()
     }

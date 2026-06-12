c6c7d343ed717d0de115b1b1cb8d98441e3a6718
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-06-12 12:37:13 +0500

    I added background with animation and function

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
     @State private var latitude = 42.3
     @State private var longitude = 69.6
     @State private var weatherEmoji = "🌤️"
+    @State private var bgColors: [Color] = [.blue, .cyan]
 
     
     
@@ -46,21 +47,27 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(
-                        colors: [.blue, .cyan],
-                        startPoint: .top,
-                        endPoint: .bottom
-                    )
-                    .ignoresSafeArea()
-
             
-            VStack {
+            LinearGradient(
+                colors: bgColors,
+                startPoint: .top,
+                endPoint: .bottom
                 
-                Picker("Город", selection: $cityName) {
+            )
+            
+            VStack(spacing: 20) {
+                Text("🌍 Weather")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.white)
+
+                Picker("The City", selection: $cityName) {
                     Text("Shymkent").tag("Shymkent")
                     Text("Almaty").tag("Almaty")
                     Text("Astana").tag("Astana")
                 }
+                
+                
                 .pickerStyle(.segmented)
                 .onChange(of: cityName) { city in
                     switch city {
@@ -76,14 +83,20 @@ struct ContentView: View {
                     }
                 }
                 
+                
+                
+                
                 Text(weatherEmoji)
                     .font(.system(size: 80))
 
                 Text(cityName)
+                    .foregroundColor(.white)
                     .font(.largeTitle)
                     .bold()
                 
                 Text(temperature)
+                    .foregroundColor(.white)
+
                     .font(.system(size:60))
                 
                 Button("find out") {
@@ -91,6 +104,7 @@ struct ContentView: View {
                     
                     
                     Task {
+                        
                         
                         
                         let url = URL(string: "https://api.open-meteo.com/v1/forecast?latitude=\(latitude)&longitude=\(longitude)&current_weather=true")!
@@ -105,6 +119,17 @@ struct ContentView: View {
                             
                                     temperature = "\(weather.current_weather.temperature)°C"
                             weatherEmoji = weatherIcon(weather.current_weather.weathercode)
+                            
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                switch weather.current_weather.weathercode {
+                                case 0: bgColors = [.orange, .yellow]      // ☀️ ясно
+                                case 1, 2, 3: bgColors = [.blue, .cyan]    // ⛅️ облачно
+                                case 61, 63, 65: bgColors = [.gray, .blue] // 🌧️ дождь
+                                case 71, 73, 75: bgColors = [.white, .gray]// ❄️ снег
+                                case 95: bgColors = [.black, .gray]        // ⛈️ гроза
+                                default: bgColors = [.blue, .cyan]
+                                }
+                            }
 
                             
                                 }
